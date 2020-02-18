621d57cd822c49758301178e8513025e82162415
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-02-18 14:47:47 -0700

    Write tests for recipe-test.js

--- test/recipe-test.js
+++ test/recipe-test.js
@@ -6,27 +6,81 @@ import ingredientsData from '../src/data/ingredients.js';
 
 let recipe;
 
-describe('Recipe', () => {
-  beforeEach(() => {
-
-    recipe = new Recipe(recipeData[47], ingredientsData);
-  });
-
-  describe('Recipe Data', () => {
-
-    it('Should hold its own ingredient data', () => {
-      expect(recipe.ingredients).to.equal(recipeData[47].ingredients);
-    })
-
-    it('Should hold its own instruction data', () => {
-      expect(recipe.instructions).to.equal(recipeData[47].instructions);
-    })
-  })
-
-  it('Should be able to calculate the cost of its ingredients', () => {
-    // console.log(ingredientsData);
-    expect(recipe.calculateCost()).to.equal(4166);
-  });
-
-
+beforeEach(() => {
+  recipe = new Recipe(recipeData[47], ingredientsData);
 });
+
+describe('Recipe', () => {
+
+    it.skip('should be an instance of recipe', () => {
+
+    });
+
+    it.skip('should be have an id', () => {
+
+    });
+
+    it.skip('should be have a name', () => {
+
+    });
+    //
+    it.skip('Should hold its own ingredient data', () => {
+      // expect(recipe.ingredients).to.equal(recipeData[47].ingredients);
+    });
+    //
+
+    describe('ingredients data', () => {
+        it.skip('should be an object', () => {
+
+        });
+    //
+        it.skip('should have a name', () => {
+
+        });
+    //
+        it.skip('should have an id', () => {
+
+        });
+
+        it.skip('should have a quantity', () => {
+
+        });
+        describe('quantity', () => {
+          it.skip('should have an amount', () => {
+
+          });
+
+          it.skip('should have a unit', () => {
+
+          });
+    
+        });
+
+      it.skip('should have instructions', () => {
+
+      });
+        describe('instructions', () => {
+          it.skip('should be an object', () => {
+
+          })
+
+          it.skip('should have a number', () => {
+
+          });
+
+          it.skip('should an instruction', () => {
+
+          })
+        });
+
+        it.skip('should have tags that are an array of strings', () => {
+
+        });
+
+        it.skip('should have have an image', () => {
+
+        });
+
+
+  })
+})
