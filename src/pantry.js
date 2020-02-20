5d362cabaaf48f5152bc3f5de7dff3276300aafc
Olivia Webster <oliviacweb@gmail.com>
2020-02-20 16:08:15 -0700

    Attempt to pass pantry method

--- src/pantry.js
+++ src/pantry.js
@@ -3,27 +3,41 @@ class Pantry {
     this.contents = userIngredients;
     this.allIngredients = allIngredients;
   }
- 
+
   evaluatePantryForRecipe(recipe) {
     let result;
-    const contents = this.contents.map(content => {
-      return content.ingredient
-    })
+    let ingredientsAvailable = [];
+    // const contents = this.contents.map(content => {
+    //   return content.ingredient
+    // })
     const recipeIngredients = recipe.ingredients.map(ingredient => {
       return ingredient.id
     })
-   
-    const ingredientEvaluation = contents.forEach(content => {
-      if (recipeIngredients.includes(content)) {
-        result = true;
-      } else {
-        result = false;
+
+    const ingredientEvaluation = this.contents.forEach(content => {
+      if (recipeIngredients.includes(content.ingredient)) {
+        ingredientsAvailable.push(content)
+        result = ingredientsAvailable;
+      }
+      if(!recipeIngredients.includes(content.ingredient)) {
+        result = 'You dont have enough ingredients for this recipe';
       }
     })
+
     return result
+
   }
+
+   determineAmountOfIngredientsMissing(recipe) {
+       //if the recipe ingredient ID matches the pantry ingredient ID return 
+
+
+       //iterate over the ingredients and
+       //
+       //return value is an object and the object is going to have ingredient id as
+   }
+
 
 }
 
 export default Pantry;
- 
